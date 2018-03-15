9e3428b6e7e2917d33e9e18f234ee93383971b19
tcob <briantcob@gmail.com>
2018-03-15 23:02:59 +0100

    satisfy the cop

--- lib/orgbots.rb
+++ lib/orgbots.rb
@@ -3,22 +3,22 @@ require 'octokit'
 require 'dotenv/load'
 
 Dotenv.load('orgbot.env')
-@client = Octokit::Client.new(:access_token => ENV['OCTOKIT_TOKEN'])
+@client = Octokit::Client.new(access_token: ENV['OCTOKIT_TOKEN'])
 
 # Load some repo configs
-repo   = @client.repo (ENV['REPO'])
+repo = @client.repo ENV['REPO']
 # add a var here for branch name
 ref = 'heads/master'
 
 begin
-  file_info = @client.contents(repo.full_name, :path => 'README.md', :branch => 'master').to_hash
+  file_info = @client.contents(repo.full_name, path: 'README.md', branch: 'master').to_hash
   puts "\n\n👌 Looks good so far"
-rescue Octokit::NotFound => e  
+rescue Octokit::NotFound => e
   puts "😨 #{file} not found in #{repo.full_name}"
   puts e.message
   # you can call create method and then exit
   exit
-rescue Exception => e
+rescue StandardError => e
   puts "\n\n🔥🔥🔥🔥🔥 You started a fire! 🔥🔥🔥🔥🔥\n\n"
   puts "An error of type #{e.class} happened, message is #{e.message}"
   exit
@@ -28,22 +28,26 @@ end
 # pp "Content before commit : #{file_info[:path]}"
 # pp Base64.decode64(file_info[:content]).delete("\n")
 
-new_content        = "much wowness"
+new_content        = 'much wowness'
 
 sha_latest_commit  = @client.ref(repo.full_name, ref).object.sha
 sha_base_tree      = @client.commit(repo.full_name, sha_latest_commit).commit.tree.sha
-blob_sha           = @client.create_blob(repo.full_name, Base64.encode64(new_content), "base64")
+blob_sha           = @client.create_blob(repo.full_name, Base64.encode64(new_content), 'base64')
 
-sha_new_tree       = @client.create_tree(repo.full_name, 
-                                   [ { :path => file_info[:path], 
-                                       :mode => "100644", 
-                                       :type => "blob", 
-                                       :sha => blob_sha } ], 
-                                   {:base_tree => sha_base_tree }).sha
+sha_new_tree       = @client.create_tree(repo.full_name,
+                                         [{ path: file_info[:path],
+                                            mode: '100644',
+                                            type: 'blob',
+                                            sha: blob_sha }],
+                                         base_tree: sha_base_tree).sha
 
-commit_message      = "👮 WebDevBot always commits directly to master"
-sha_new_commit      = @client.create_commit(repo.full_name, commit_message, sha_new_tree, sha_latest_commit).sha
+commit_message      = '👮 WebDevBot always commits directly to master'
+sha_new_commit      = @client.create_commit(repo.full_name,
+                                            commit_message, sha_new_tree, sha_latest_commit).sha
 
 updated_ref         = @client.update_ref(repo.full_name, ref, sha_new_commit)
+
+# satify the cops - useless variable assignment
+puts updated_ref
 
 pp "📈 Content updated : #{file_info[:path]}"
--- orgbots.gemspec
+++ orgbots.gemspec
@@ -30,10 +30,10 @@ Gem::Specification.new do |spec|
   spec.require_paths = ['lib']
 
   spec.add_development_dependency 'bundler', '~> 1.16'
+  spec.add_development_dependency 'dotenv'
   spec.add_development_dependency 'octokit', '~> 4.0'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
   spec.add_development_dependency 'rubocop'
   spec.add_development_dependency 'rubocop_rules'
-  spec.add_development_dependency 'dotenv'
 end
