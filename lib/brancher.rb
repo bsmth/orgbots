dfff2d9ef2be45beed9a323167d529c2a9042784
tcob <briantcob@gmail.com>
2018-03-18 21:10:38 +0100

    minor updates

--- lib/brancher.rb
+++ lib/brancher.rb
@@ -2,29 +2,28 @@ require 'octokit'
 
 # Creates a branch in given repo
 class BranchBot
-  def initialize(r, b, t)
+  def initialize(r, t)
     @c = Octokit::Client.new(access_token: t)
-    @c.auto_paginate = true
     @repo = r
-    @branch = b
-    @ref = "heads/#{@branch}"
-    @repo_branches = @c.branches(@repo).collect { |br| br[:name] }
   end
 
-  def create_branch
-    if @repo_branches.include?(@branch)
-      puts "Branch #{@branch} already exists in #{@repo}"
+  def list_branches
+    @c.branches(@repo).collect { |br| br[:name] }
+  end
+
+  def create_branch(branch, base)
+    if list_branches.include?(branch)
+      puts "Branch #{branch} already exists in #{@repo}"
     else
-      # branch from master?
-      ref = @c.ref(@repo, 'heads/master')
+      ref = @c.ref(@repo, "heads/#{base}")
       sha = ref[:object][:sha]
-      create_ref = @c.create_ref(@repo, 'heads/' + @branch, sha)
+      create_ref = @c.create_ref(@repo, 'heads/' + branch, sha)
       puts "Created branch #{create_ref[:ref]}"
     end
   end
 
-  def delete_branch
-    puts "Deleting #{@branch} from #{@repo}"
-    @c.delete_ref(@repo, 'heads/' + @branch) if @repo_branches.include?(@branch)
+  def delete_branch(branch)
+    @c.delete_ref(@repo, 'heads/' + branch) if @repo_branches.include?(branch)
+    puts "Deleting #{branch} from #{@repo}"
   end
 end
