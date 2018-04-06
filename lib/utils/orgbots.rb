e081164e1e3a352c38e2f8c247400ef15bd1ff0e
tcob <briantcob@gmail.com>
2018-04-07 00:48:27 +0200

    add some minor garbling

--- lib/utils/orgbots.rb
+++ lib/utils/orgbots.rb
@@ -1,20 +1,19 @@
 require 'octokit'
 require 'dotenv/load'
 require 'base64'
-require_relative 'orgbots/version'
+require_relative 'util'
 require_relative 'pullrequest'
 require_relative 'committer'
 require_relative 'brancher'
 require_relative 'issues'
 require_relative 'files'
 require_relative 'reacter'
-require_relative 'util'
 
 Dotenv.load('orgbot.env')
 r = ENV['REPO']
 t = ENV['TOKEN']
 
-puts FileBot.new(r, t).list_files('master').inspect
+# puts FileBot.new(r, t).list_files('master').inspect
 # randombranch = BranchBot.new(r,t).list_branches.sample
 # puts Octokit.contents(r).inspect
 
