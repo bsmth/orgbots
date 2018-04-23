04c8cea1a00894ebb3e2ef0a2fb9156fabd31f96
tcob <briantcob@gmail.com>
2018-04-24 00:16:02 +0200

    updating swag

--- test/test_branches.rb
+++ test/test_branches.rb
@@ -5,6 +5,15 @@ require_relative '../lib/utils/brancher'
 Dotenv.load('orgbot.env')
 
 class TestBranches < Test::Unit::TestCase
+  def test_create_delete_branch
+    brancher = BranchBot.new(ENV['REPO'], ENV['TOKEN'])
+    assert_nothing_raised do
+      brancher.create_branch('cooltestbranch', 'master')
+      sleep 10
+      brancher.delete_branch('cooltestbranch')
+    end
+  end
+
   def test_list_branches
     brancher = BranchBot.new(ENV['REPO'], ENV['TOKEN'])
     assert_nothing_raised do
